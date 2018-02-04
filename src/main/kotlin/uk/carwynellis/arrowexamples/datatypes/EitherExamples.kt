3d9f49492bc003c48d9dd8ac91d9f064c25b3e21
Carwyn Ellis <carwynellis@gmail.com>
2018-02-04 20:34:41 +0000

    Completed Either example

--- src/main/kotlin/uk/carwynellis/arrowexamples/datatypes/EitherExamples.kt
+++ src/main/kotlin/uk/carwynellis/arrowexamples/datatypes/EitherExamples.kt
@@ -1,7 +1,16 @@
 package uk.carwynellis.arrowexamples.datatypes
 
-import arrow.*
 import arrow.core.*
+import arrow.syntax.applicative.tupled
+import arrow.syntax.either.left
+import arrow.syntax.either.right
+import arrow.typeclasses.binding
+
+// Error class for Either with ADT example.
+sealed class Error {
+    object NotANumber : Error()
+    object NoZeroReciprocal : Error()
+}
 
 /**
  * Source http://arrow-kt.io/docs/datatypes/either/
@@ -97,7 +106,8 @@ fun main(args: Array<String>) {
     println("reciprocal of 12 is ${exceptionStyle.reciprocal(12)}")
     println("stringify of 12.0 returns ${exceptionStyle.stringify(12.0)}")
 
-    // Instead, let’s make the fact that some of our functions can fail explicit in the return type.
+    // Instead, let’s make the fact that some of our functions can fail explicit
+    // in the return type.
 
     // Either Style
 
@@ -128,4 +138,159 @@ fun main(args: Array<String>) {
     println("magic of 0 returns: ${eitherStyle.magic("0")}")
     println("magic of 1 returns: ${eitherStyle.magic("1")}")
     println("magic of 'foo' returns: ${eitherStyle.magic("foo")}")
+
+    // In the following exercise we pattern-match on every case the Either
+    // returned by magic can be in. Note the when clause in the Left - the
+    // compiler will complain if we leave that out because it knows that given
+    // the type Either[Exception, String], there can be inhabitants of Left that
+    // are not NumberFormatException or IllegalArgumentException. You should
+    // also notice that we are using SmartCast for accessing to Left and Right
+    // value.
+
+    val x = eitherStyle.magic("2")
+
+    val value = when(x) {
+        is Either.Left -> when (x.a){
+            is NumberFormatException -> "Not a number!"
+            is IllegalArgumentException -> "Can't take reciprocal of 0!"
+            else -> "Unknown error"
+        }
+        is Either.Right -> "Got reciprocal: ${x.b}"
+    }
+
+    println("magic('2') returned reciprocal of $value")
+
+    // Instead of using exceptions as our error value, let’s instead enumerate
+    // explicitly the things that can go wrong in our program.
+
+    val eitherWithAdtStyle = object {
+        fun parse(s: String): Either<Error, Int> =
+            if (s.matches(Regex("-?[0-9]+"))) Either.Right(s.toInt())
+            else Either.Left(Error.NotANumber)
+
+        fun reciprocal(i: Int): Either<Error, Double> =
+            if (i == 0) Either.Left(Error.NoZeroReciprocal)
+            else Either.Right(1.0 / i)
+
+        fun stringify(d: Double): String = d.toString()
+
+        fun magic(s: String): Either<Error, String> =
+            parse(s).flatMap{reciprocal(it)}.map{stringify(it)}
+    }
+
+    // For our little module, we enumerate any and all errors that can occur.
+    // Then, instead of using exception classes as error values, we use one of
+    // the enumerated cases. Now when we pattern match, we get much nicer
+    // matching. Moreover, since Error is sealed, no outside code can add
+    // additional subtypes which we might fail to handle.
+
+    val y = eitherWithAdtStyle.magic("2")
+
+    when(y) {
+        is Either.Left -> when (y.a){
+            is Error.NotANumber -> "Not a number!"
+            is Error.NoZeroReciprocal -> "Can't take reciprocal of 0!"
+        }
+        is Either.Right -> "Got reciprocal: ${y.b}"
+    }
+
+    println("magic('2') using Either/ADT style returned reciprocal of $value")
+
+    // Additional Syntax
+
+    // Either can also map over the left value with mapLeft which is similar to
+    // map but applies on left instances.
+
+    val r : Either<Int, Int> = Either.Right(7)
+    val rMapLeft = r.mapLeft{it +1}
+    println("mapping a right with mapLeft returns: $rMapLeft")
+
+    val l: Either<Int, Int> = Either.Left(7)
+    val lMapLeft = l.mapLeft{it + 1}
+    println("mapping a left with mapLeft returns: $lMapLeft")
+
+    // Either<A, B> can be transformed to Either<B,A> using the swap() method.
+
+    val r2: Either<String, Int> = Either.Right(7)
+    val r2Swapped = r2.swap()
+    println("swapping $r2 returns: $r2Swapped")
+
+    // For using Either’s syntax on arbitrary data types arrow provides
+    // additional syntax which can be imported. This provides left(), right(),
+    // contains() and getOrElse() methods.
+    // TODO - getOrHandle referenced in the documentation appears to have been
+    //        removed.
+
+    val rightOfNumber = 7.right()
+
+    println("7.right() returns: $rightOfNumber")
+
+    val leftOfString = "hello".left()
+
+    println("\"hello\".left() returns $leftOfString")
+
+    val rightContainsSeven = rightOfNumber.contains(7)
+
+    println("7.right() contains 7: $rightContainsSeven")
+
+    val getOrElseOfLeft = leftOfString.getOrElse { 7 }
+
+    println("getOrElse on left returned: $getOrElseOfLeft")
+
+    // To create an Either instance using a predicate, use the Either.cond()
+    // method.
+    val predicateTrue = Either.cond(true, { 42 }, { "Error" })
+
+    println("Either.cond with predicate true returned: $predicateTrue")
+
+    val predicateFalse = Either.cond(true, { 42 }, { "Error" })
+
+    println("Either.cond with predicate false returned: $predicateFalse")
+
+    // Another operation is fold. This operation will extract the value from
+    // the Either, or provide a default if the value is Left.
+
+    val foldOnRight = 7.right().fold( { 1 }, { it * 2 })
+
+    println("foldOnRight returned: $foldOnRight")
+
+    // TODO - this only worked with explicit type on multiply function.
+    val foldOnLeft = 7.left().fold( { 1 }, { n: Int -> n * 2 })
+
+    println("foldOnLeft returned $foldOnLeft")
+
+    // Arrow contains Either instances for many useful typeclasses that
+    // allows you to use and transform right values. Both Option and Try don’t
+    // require a type parameter with the following functions, but it is
+    // specifically used for Either.Left
+
+    // Functor
+
+    // Transforming the inner contents
+
+    val eitherFunctor = Either.functor<Int>().map(Either.Right(1), {it + 1})
+
+    println("Either.functor example returned: $eitherFunctor")
+
+    // Applicative
+
+    // Computing over independent values
+
+    val eitherApplicative = Either.applicative<Int>()
+        .tupled(Either.Right(1), Either.Right("a"), Either.Right(2.0))
+
+    println("Either.applicative example returned: $eitherApplicative")
+
+    // Monad
+
+    // Computing over dependent values ignoring absence
+
+    val eitherMonad = Either.monad<Int>().binding {
+        val a = Either.Right(1).bind()
+        val b = Either.Right(1 + a).bind()
+        val c = Either.Right(1 + b).bind()
+        yields(a + b + c)
+    }
+
+    println("Either.monad example returned: $eitherMonad")
 }
